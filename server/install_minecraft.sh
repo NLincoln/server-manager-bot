28c8e2fe6c27bd554924f3090654a83580cbb6c6
Nathan Lincoln <lincolnnathan205@gmail.com>
2021-01-08 15:59:19 -0600

    Make the AMI image generic to the modpack

--- server/install_minecraft.sh
+++ server/install_minecraft.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-modpack_id=52
-modpack_version=216
-
-installer_download_url="https://api.modpacks.ch/public/modpack/$modpack_id/$modpack_version/server/linux"
+installer_download_url="https://api.modpacks.ch/public/modpack/$MODPACK_ID/$MODPACK_VERSION/server/linux"
 username="ubuntu"
 home_dir=/home/$username
 
@@ -16,9 +13,9 @@ installer_name=modpack_installer
 installer_location=$home_dir/$installer_name
 
 
-curl $installer_download_url -o $installer_location
+curl "$installer_download_url" -o $installer_location
 chmod +x $installer_location
 
 mkdir $modpack_folder
 
-./$installer_name $modpack_id $modpack_version --path $modpack_folder
+./$installer_name "$MODPACK_ID" "$MODPACK_VERSION" --path $modpack_folder
